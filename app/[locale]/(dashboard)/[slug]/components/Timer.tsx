88e8e2115443e5c3df061b6bcc7f3f8daeb9f871
Saman <samanezzatabadi@yahoo.com>
2025-09-16 19:44:43 +0330

    hope it helps

--- app/[locale]/(dashboard)/[slug]/components/Timer.tsx
+++ app/[locale]/(dashboard)/[slug]/components/Timer.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 function Timer({ slug }: { slug: string }) {
   const [timeLeft, setTimeLeft] = useState(5);
   const t = useTranslations("Details");
+
+  const locale = useLocale()
 
   useEffect(() => {
     if (timeLeft <= 0) return; // stop when reaches 0
@@ -23,7 +25,7 @@ function Timer({ slug }: { slug: string }) {
       {timeLeft > 0 ? (
         <div className="text-center text-gray-600">Please wait <span className="font-bold">{timeLeft}</span> seconds to continue</div>
       ) : (
-        <Link href={`/${slug}/download`} className="bg-[#5AB696] font-bold text-white rounded-xl px-7 py-3 mt-10">
+        <Link locale={locale} href={`/${slug}/download`} className="bg-[#5AB696] font-bold text-white rounded-xl px-7 py-3 mt-10">
           {t("freeDownload")}
         </Link>
       )}
